a71d21f2c23ec14af8d512d495c8458d3aabd927
heayriwang <118506446+heayriwang@users.noreply.github.com>
2022-12-01 17:45:39 +0900

    22.12.01
    포르쉐페이지 만들기

--- js/porsche.js
+++ js/porsche.js
@@ -21,4 +21,67 @@ $(function () {
             .siblings().removeClass('on');
         $('.mainVisual .main_num').text(c + 1)
     });
+
+    $('.main_dots li').on('click', function () {
+        var idx = $(this).index();
+        $('.mainSlide').slick('slickGoTo', idx)
+    })
+
+
+    $('.main_pro_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 5,
+    });
+
+    $('.mainProduct .arrows .prev').on('click', function () {
+        $('.main_pro_slide').slick('slickPrev')
+    });
+
+    $('.mainProduct .arrows .next').on('click', function () {
+        $('.main_pro_slide').slick('slickNext')
+    });
+
+    $('.mainCustomer .tab_menu>li').on('click', function (event) {
+        event.preventDefault();
+        var _this = $(this); //이벤트가 적용된 자신을 반환
+        var idx = $(this).index(); //이벤트가 적용된 자신의 번호를 반환 0,1,2
+        $(this).addClass('on').siblings().removeClass('on')
+        $('.mainCustomer .tab_content>li').eq(idx).addClass('on').siblings().removeClass('on')
+    });
+
+    $('#f_link').on('change', function () {
+        var link = $(this).val();
+        link && window.open(link);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
